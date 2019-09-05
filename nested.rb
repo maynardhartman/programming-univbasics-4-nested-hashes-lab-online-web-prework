d8c0d4ce22a7d384681cec0da0270b9a087e8cb4
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-05 09:26:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -101,7 +101,7 @@ def changing_alan
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+        }filter_parameter_logging
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
