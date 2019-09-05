f79f720e7938ce7e6a69777984a88ec9ca6f806d
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-05 09:26:38 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -36,7 +36,9 @@ def alan_kay_is_known_for
           :known_for => "Unix",
           :languages => ["C"]
         }
+    
      }
+     return([:alan_kay][:known_for])
 end
 
 def dennis_ritchies_language
@@ -77,10 +79,12 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+        },
+        :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+         }
      }
-
-    
 end
 
 def changing_alan
@@ -91,13 +95,13 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
+          :known_for => ["GUI"]
+          :languages => ["Smalltalk","LISP"]
         },
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+        }filter_parameter_logging
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
@@ -113,7 +117,7 @@ def adding_to_dennis
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
